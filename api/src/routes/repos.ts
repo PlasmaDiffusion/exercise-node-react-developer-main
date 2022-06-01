1dd5a8c26952d0ff2817efaed8d43a7137ee4884
Scott Cooper <Scott50000@gmail.com>
2022-06-01 16:12:04 -0400

    Get data from github url using axios

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from 'express';
 import fs from 'fs';
+import axios from 'axios';
 
 export const repos = Router();
+
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -13,9 +15,18 @@ repos.get('/', async (_: Request, res: Response) => {
       throw err;
     }
     const repo = JSON.parse(data);
-    console.log(repo);
+    // console.log(repo);
   });
 
+  await axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((response: { data: any }) => {
+      console.log(response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((err: any) => {
+      res.status(500).json({ message: err });
+    });
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   res.json([]);
