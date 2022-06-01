1dba1ec4a91597ed42a739309dc03154f4906ed7
Scott Cooper <Scott50000@gmail.com>
2022-06-01 17:41:18 -0400

    Basic response error handling

--- web/src/components/RepoList.tsx
+++ web/src/components/RepoList.tsx
@@ -3,9 +3,16 @@ import axios from 'axios';
 
 function RepoList() {
   useEffect(() => {
-    axios.get('http://localhost:4000/repos').then((res) => {
-      console.log(res);
-    });
+    axios
+      .get('http://localhost:4000/repos')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        // Just reload the page if there's an error
+        console.log(err);
+        window.location.reload();
+      });
   }, []);
 
   return <>Test</>;
