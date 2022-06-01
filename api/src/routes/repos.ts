0e6806a42de9ab495554ff1618ecb8ab9064f51d
Scott Cooper <Scott50000@gmail.com>
2022-06-01 15:53:09 -0400

    Read in repos.json file

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs';
 
 export const repos = Router();
 
@@ -6,6 +7,14 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
+
+  fs.readFile('data/repos.json', (err: any, data: any) => {
+    if (err) {
+      throw err;
+    }
+    const repo = JSON.parse(data);
+    console.log(repo);
+  });
 
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
